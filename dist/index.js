aff33ff7fdecb9b3e63e9fd19b0991e1a03040e6
y3dc <vungocduyet24@gmail.com>
2023-12-16 16:39:54 +0700

    add proto files

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,10 @@ var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
 var __copyProps = (to, from, except, desc) => {
   if (from && typeof from === "object" || typeof from === "function") {
     for (let key of __getOwnPropNames(from))
@@ -15,4 +19,37 @@ var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: tru
 
 // src/index.ts
 var src_exports = {};
+__export(src_exports, {
+  AUTH_PACKAGE_NAME: () => AUTH_PACKAGE_NAME,
+  AUTH_SERVICE_NAME: () => AUTH_SERVICE_NAME,
+  AuthServiceControllerMethods: () => AuthServiceControllerMethods,
+  protobufPackage: () => protobufPackage
+});
 module.exports = __toCommonJS(src_exports);
+
+// src/proto/auth.ts
+var import_microservices = require("@nestjs/microservices");
+var protobufPackage = "auth";
+var AUTH_PACKAGE_NAME = "auth";
+function AuthServiceControllerMethods() {
+  return function(constructor) {
+    const grpcMethods = ["authenticate"];
+    for (const method of grpcMethods) {
+      const descriptor = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      (0, import_microservices.GrpcMethod)("AuthService", method)(constructor.prototype[method], method, descriptor);
+    }
+    const grpcStreamMethods = [];
+    for (const method of grpcStreamMethods) {
+      const descriptor = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      (0, import_microservices.GrpcStreamMethod)("AuthService", method)(constructor.prototype[method], method, descriptor);
+    }
+  };
+}
+var AUTH_SERVICE_NAME = "AuthService";
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  AUTH_PACKAGE_NAME,
+  AUTH_SERVICE_NAME,
+  AuthServiceControllerMethods,
+  protobufPackage
+});
